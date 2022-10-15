6a61ec939badb9471ec49d3a0a69c14605e1c0ab
fs-nathan <nathan.makeitpro@gmail.com>
2022-10-15 13:39:03 +0700

    Update config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,8 @@
 export const AppConfig = {
   site_name: 'Taxi Thành Luân',
-  title: 'Taxi Thành Luân',
-  description: 'Taxi Thành Luân chuyên tuyến Hà Nội đi Nội Bài, các tỉnh',
+  title: 'Taxi Thành Luân - Đặt xe gọi ngay 0917812866',
+  description:
+    'Taxi Thành Luân chuyên tuyến Hà Nội đi Nội Bài, di chuyển đến các tỉnh khắp cả nước',
   locale: 'vi',
+  contact_phone: '0917812866',
 };
