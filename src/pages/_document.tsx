4d35d562bda13b2279f6274791d0164bf98c91c8
fs-nathan <nathan.makeitpro@gmail.com>
2022-10-27 16:58:31 +0700

    Add GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,27 @@ class MyDocument extends Document {
             name="google-site-verification"
             content="q8Pd4HWDL36_2_UMDJtz943O3Iw5QnBMBH35dlui4EY"
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
