110fe33b5cab67f1754e106fba36c1d2b3edfe14
fs-nathan <nathan.makeitpro@gmail.com>
2022-10-31 00:50:53 +0700

    Fix mail

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,19 +1,23 @@
-import {
-  sendEmail,
-  generateBookingHTMLEmailContent,
-} from '../../utils/mail/mailjet';
+import { sendEmail } from '../../utils/mail/mailjet';
 
 export default async function handler(req: any, res: any) {
   if (req.method === 'POST') {
     // Process a POST request
-    const book = req.body;
+    // const book = req.body;
     const response = await sendEmail(
       '',
       '',
       '',
       '',
-      generateBookingHTMLEmailContent(book)
+      '[Error] Database READ exceeds quota'
     );
+    // const response = await sendEmail(
+    //   '',
+    //   '',
+    //   '',
+    //   '',
+    //   generateBookingHTMLEmailContent(book)
+    // );
     res.status(200).json({ response });
   } else {
     res.status(200).json({ message: 'Mail API works!' });
