0f45e09a6b1448de838c088ac651ad0819d1e95f
fs-nathan <99985579+fs-nathan@users.noreply.github.com>
2022-11-01 22:26:49 +0700

    Update book.tsx

--- src/form/book.tsx
+++ src/form/book.tsx
@@ -62,7 +62,7 @@ const BookForm = (props: BookFormProps) => {
     res = await res.json();
     const messages = get(res, 'response.Messages', []);
     if (messages && messages[0]) {
-      if (messages[0].Status === 'success') {
+      if (get(messages[0], 'Status') === 'success') {
         setMailSent(true);
       }
     }
